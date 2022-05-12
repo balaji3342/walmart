2a14557c13d1fa4a174b10feff263a7ca3245d7f
Cbalajireddy <balaji22885@gmail.com>
2022-05-12 12:47:32 +0530

    -revert ..Revert "update"
    This reverts commit a174000f47d37138cb45eceacb9eaa21f0d00780.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
--reverted .. revert "added new files"
-balu
-O
 this line is add for testing
 hello guys gud morning to all
-parent of 177b024... updated deploy.sh
-
 parent of 177b024... updated deploy.sh
 this file contains the deploy script
 
