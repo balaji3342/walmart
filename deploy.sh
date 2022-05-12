a174000f47d37138cb45eceacb9eaa21f0d00780
Cbalajireddy <balaji22885@gmail.com>
2022-05-12 12:46:23 +0530

    update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
+-reverted .. revert "added new files"
+balu
+O
 this line is add for testing
 hello guys gud morning to all
+parent of 177b024... updated deploy.sh
+
 parent of 177b024... updated deploy.sh
 this file contains the deploy script
 
